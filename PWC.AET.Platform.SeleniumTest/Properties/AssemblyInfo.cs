c7ded2cf9028bbfe8dce8e74f9424dfe0148e030
dushanka001 <dushanka.morris@agilethought.com>
2019-10-09 16:53:08 -0400

    Commit

---
+++ PWC.AET.Platform.SeleniumTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PWC.AET.Platform.SeleniumTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PWC.AET.Platform.SeleniumTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49b347b3-903d-48df-b838-c04d703d51ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PWC.AET.Platform.SeleniumTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PWC.AET.Platform.SeleniumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.XPath("//*[@name='q']")).SendKeys("Tampa");
+            Thread.Sleep(8000);
+            driver.FindElement(By.XPath("//*[@name='btnK']")).Click();
+            //validate
+            Assert.IsTrue(driver.FindElements(By.XPath("//h3")).Count > 0,"There were no links");
+        }
+
+
+        [TestInitialize]
+        public void Preconditioon()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestCleanup]
+        public void Postcondition()
+        {
+            driver.Close();
+        }
+    }
+}
